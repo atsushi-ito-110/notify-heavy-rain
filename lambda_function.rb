ebce51172e3d3bd94ba6076974710de86170e0f7
Atsushi Ito <atsushi.itoh66@gmail.com>
2020-09-15 14:11:49 +0900

    fix: return -> next in each

--- lambda_function.rb
+++ lambda_function.rb
@@ -15,14 +15,14 @@ def lambda_handler(event:, context:)
     logger.info(spot)
     unless spot['last_notified_at'].empty?
       logger.info('needs_notify? is false')
-      return
+      next
     end
 
     heavy_rains = weather.heavy_rains(spot['longitude'], spot['latitude'])
 
     if heavy_rains.empty?
       logger.info('heavy_rains is empty')
-      return
+      next
     end
 
     slack.notify_rains(heavy_rains)
