df172e5ae624c7053d29d931a1df26e27625926c
Atsushi Ito <atsushi.itoh66@gmail.com>
2020-09-15 15:13:25 +0900

    update: pack.sh versions archives

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-rm ./lambda_function.zip
+version_name=`date +"%Y%m%d%H%M%S".zip`
+mv ./lambda_function.zip "./versions/${version_name}"
 zip -rq lambda_function.zip lambda_function.rb vendor/ .env lib/ config/
